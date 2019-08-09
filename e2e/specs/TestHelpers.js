b8b0a30fd89afdf80a7558a0b603a55302996f89
Denis Omelkov <omelkov@skbkontur.ru>
2019-08-09 17:24:33 +0500

    dep security fixes, npmignore fixes, maintenance

--- e2e/specs/TestHelpers.js
+++ e2e/specs/TestHelpers.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var protractor_1 = require("protractor");
 function openPage() {
-    return protractor_1.browser.get('http://localhost:3000');
+    return protractor_1.browser.get('http://localhost:3000/index.html');
 }
 exports.openPage = openPage;
